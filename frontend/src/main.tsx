8e770da10ff23c90d360e33ccbcd7d6dd6de0958
deepu1304 <deepu84204@gmail.com>
2026-05-08 20:02:14 +0530

    feat: triage form + doctor dashboard with WebSocket streaming and SHAP visualization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/Login'
+import TriagePage from './pages/TriagePage'
+import DoctorDashboard from './pages/DoctorDashboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
-        {/* More routes added as we build more pages */}
+        <Route path="/triage" element={<TriagePage />} />
+        <Route path="/dashboard" element={<DoctorDashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
